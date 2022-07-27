d694893b70a22998b650f60d73ae4df382e5bbc0
James DiNapoli <james.dinapoli@outlook.com>
2022-07-27 19:16:12 -0400

    mapActions and mapState

---
+++ src/store/modules/plan.js
@@ -0,0 +1,61 @@
+import PlanService from '@/services/PlanService'
+
+export default({
+    namespaced: true,
+    state: {    
+        plans: [],
+        currentPlan: {}
+    },
+    getters: {
+    },
+    mutations: {
+        ADD_PLAN(state, plan){
+            state.plans.push(plan)
+        },
+        SET_PLANS(state, plans){
+            state.plans = plans
+        },
+        SET_PLAN(state, plan){
+            state.currentPlan = plan
+        }
+    },
+    actions: {
+        createPlan({ commit }, plan){
+            PlanService.insertPlan(plan)
+            .then(response => {
+                commit('ADD_PLAN', response.data)
+                commit('SET_PLAN', response.data)
+            })
+            .catch(error => {
+                throw(error)
+            })
+        },
+
+        /// Get all plans.
+        fetchPlans({ commit }){
+            return PlanService.getPlans()
+            .then(response => {
+                commit('SET_PLANS', response.data)
+            })
+            .catch(error => {
+                throw(error)
+            })
+        },
+
+        /// Get a plan by Id.
+        fetchPlan({ commit, state }, id){
+            const existingPlan = state.plans.find(plan => plan.id == id)
+            if(existingPlan) {
+                commit('SET_PLAN', existingPlan)
+            } else {
+                return PlanService.getPlan(id)
+                .then(response => {
+                    commit('SET_PLAN', response.data)
+                })
+                .catch(error => {
+                    throw(error)
+                })
+            }
+        }
+  }
+})
